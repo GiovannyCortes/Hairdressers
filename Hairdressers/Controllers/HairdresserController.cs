993004ec37c9e946616cd0b85090a20d1405aa04
GiovannyCortes <giovannyandres.cortes@outlook.com>
2023-03-07 13:16:03 +0100

    v1.5.0

---
+++ Hairdressers/Controllers/HairdresserController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Hairdressers.Controllers {
+    public class HairdresserController : Controller {
+
+        public IActionResult ControlPanel(string hid) {
+            ViewData["PRUEBA"] = hid;
+            return View();
+        }
+
+    }
+}
