df7b47c726ac6efc1750505c377b2cc71f645e2a
GiovannyCortes <giovannyandres.cortes@outlook.com>
2023-03-23 14:16:06 +0100

    v6.3.0

--- Hairdressers/Helpers/HelperEmailService.cs
+++ Hairdressers/Helpers/HelperEmailService.cs
@@ -43,60 +43,6 @@ namespace Hairdressers.Helpers {
         }
 
 
-        public int SendConfirmationEmailConArchivoAniadido(string receiver, string email) {
-            /*Random random = new Random();
-            int verification_code = random.Next(100000, 1000000);
-
-             Create text content
-            TextPart cuerpo = new TextPart("html") {
-                Text = @"<html>
-                              <head>
-                                <meta charset='UTF-8'>
-                              </head>
-                              <body>
-                                <h2 style='text-aling:center;'>Verificación de la cuenta de correo electrónico</h2>
-                                <p>Su código de verificación es: <strong>" + verification_code + @"</strong></p>
-                                <p>Vuelva a ventana de la aplicación e introduzca dicho código para verificar su cuenta</p>
-                                <img src=""cid:Company.jpg"" alt='Company banner image' style=""width:100%;""/>
-                              </body>
-                            </html>"
-            };
-
-           Create an image attachment for the file located at path
-           var attachment = new MimePart("image", "jpg") {
-               Content = new MimeContent(File.OpenRead("wwwroot/images/company_banner.jpg"), ContentEncoding.Default),
-               ContentDisposition = new ContentDisposition(ContentDisposition.Attachment),
-               ContentTransferEncoding = ContentEncoding.Base64,
-               FileName = Path.GetFileName("wwwroot/images/company_banner.jpg")
-           };
-
-             Create the multipart/mixed container to hold the message text and the image attachment
-            var multipart = new Multipart("mixed");
-                multipart.Add(cuerpo);
-                multipart.Add(attachment);
-
-
-             Configuramos el mensaje a enviar
-            var mensaje = new MimeMessage();
-                mensaje.From.Add(new MailboxAddress(this._sender, this._email));
-                mensaje.To.Add(new MailboxAddress(receiver, email));
-                mensaje.Subject = "Cut&Go: Código de verificación";
-                mensaje.Body = multipart;
-
-             Configuramos la conexión SMTP
-            using (var clienteSmtp = new MailKit.Net.Smtp.SmtpClient()) {
-                clienteSmtp.Connect(this._host, this._port, false);
-                clienteSmtp.Authenticate(this._email, this._password);
-
-                 Envíamos el mensaje y desconectamos el servicio
-                clienteSmtp.Send(mensaje);
-                clienteSmtp.Disconnect(true);
-
-                return verification_code;
-            }*/
-            return 1;
-        }
-
         /*public int SendConfirmationEmail(string receiver, string email) {
             Random random = new Random();
             int verification_code = random.Next(100000, 1000000);
